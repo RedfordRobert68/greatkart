a7cec3c42fb3b6b998bb5440db0956e0cdbc73e3
RedfordRobert68 <redfordrobert68@gmail.com>
2025-08-17 19:09:16 -0400

    Updated ALLOWED_HOSTS is conditional

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -50,12 +50,18 @@ DEBUG = env.bool("DEBUG", default=False)
 # DEBUG = env('DEBUG')
 # DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [
-    ".elasticbeanstalk.com",  # allows EB environment URL
-    ".compute.amazonaws.com", # allows EC2/ELB internal checks
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# Fallback: allow health checker & localhost
+if not ALLOWED_HOSTS or ALLOWED_HOSTS == ['']:
+    ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
+
+# ALLOWED_HOSTS = [
+#     ".elasticbeanstalk.com",  # allows EB environment URL
+#     ".compute.amazonaws.com", # allows EC2/ELB internal checks
+#     "localhost",
+#     "127.0.0.1",
+# ]
 
 # ALLOWED_HOSTS = [
 #     ".elasticbeanstalk.com",
