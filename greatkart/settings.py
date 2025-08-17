463b7d44378055cc89a1c030a6ce832625e60f87
RedfordRobert68 <redfordrobert68@gmail.com>
2025-08-17 16:34:37 -0400

    edits to email defaults

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -192,10 +192,10 @@ SESSION_TIMEOUT_REDIRECT = '/accounts/login'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = env('EMAIL_HOST', default='localhost')
-EMAIL_PORT = env('EMAIL_PORT', default='587', cast=int)
+EMAIL_PORT = env.int('EMAIL_PORT', default=587)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
-EMAIL_USE_TLS = env('EMAIL_USE_TLS', default =False, cast=bool)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default =False)
 EMAIL_BACKEND = env('EMAIL_BACKEND')
 
 
